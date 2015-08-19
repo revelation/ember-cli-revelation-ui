f19aa11041f7be6317a99e3f6b56400cfffe66e9
Kyle <kyle.scherling@revelationglobal.com>
2015-08-19 09:37:31 -0700

    Gets addon back to buildable state, dev flag set to true for automamtic compilation

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-revelation-ui'
+  name: 'ember-cli-revelation-ui',
+
+  // Automatically rebuilds on change
+  isDevelopingAddon: function() {
+    return true;
+  }
+
 };
