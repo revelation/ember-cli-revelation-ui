c3f3a45d3e3023cd8083af40b618a1fdde450a64
Kyle <kyle.scherling@revelationglobal.com>
2015-08-21 12:21:37 -0700

    Finalizes styling coverage to button component

--- tests/unit/components/rui-button-test.js
+++ tests/unit/components/rui-button-test.js
@@ -61,7 +61,7 @@ test('it adds style class when style property is set', function(assert) {
   assert.ok(this.$().hasClass('btn-link'), 'it should have \'btn-link\' class when style is set to \'link\'');
 });
 
-test('it changes size when size property is set', function(assert) {
+test('it adds size class when size property is set', function(assert) {
   assert.expect(3);
   var component = this.subject();
   this.render();
@@ -80,4 +80,31 @@ test('it changes size when size property is set', function(assert) {
     component.set('size', 'xs');
   });
   assert.ok(this.$().hasClass('btn-xs'), 'it should have \'btn-xs\' class when size is set to \'xs\'');
+});
+
+test('it displays block class when block property is set', function(assert) {
+  assert.expect(2);
+  var component = this.subject();
+  this.render();
+
+  assert.notOk(this.$().hasClass('btn-block'), 'it should not have \'btn-block\' by default');
+
+  Ember.run(function(){
+    component.set('block', true);
+  });
+  assert.ok(this.$().hasClass('btn-block'), 'it should have \'btn-block\' class when block is set to \'true\'');
+});
+
+test('it disables when disable property is set to true', function(assert) {
+  assert.expect(2);
+  var component = this.subject();
+  this.render();
+
+  assert.notEqual(this.$().attr('disabled'), 'disabled', 'it should not be disabled by default');
+
+  Ember.run(function(){
+    component.set('disabled', true);
+  });
+
+  assert.equal(this.$().attr('disabled'), 'disabled', 'it should be disabled when disabled property is set to \'true\'');
 });
