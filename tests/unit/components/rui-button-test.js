3d0bc6ee9f8cbc446a2bbfab2fa31f663eb99ad9
Kyle <kyle.scherling@revelationglobal.com>
2015-08-21 12:08:30 -0700

    Adds more unit tests

--- tests/unit/components/rui-button-test.js
+++ tests/unit/components/rui-button-test.js
@@ -18,17 +18,66 @@ test('it renders', function(assert) {
   assert.equal(this.$().text().trim(), '');
 });
 
-test('it changes color when style property is set', function(assert) {
-  assert.expect(1);
+test('it adds style class when style property is set', function(assert) {
+  assert.expect(8);
   var component = this.subject();
-
   this.render();
-  var color1 = this.$().css('background-color');
+
+  assert.ok(this.$().hasClass('btn-default'), 'it should default to \'btn-default\' class when no style is set to \'primary\'');
+
+  Ember.run(function(){
+    component.set('style', 'wrongstyle');
+  });
+  assert.ok(this.$().hasClass('btn-default'), 'it should default to \'btn-default\' class when (unsupported) style is set to \'wrongstyle\'');
 
   Ember.run(function(){
     component.set('style', 'primary');
   });
-  var color2 = this.$().css('background-color');
+  assert.ok(this.$().hasClass('btn-primary'), 'it should have \'btn-primary\' class when style is set to \'primary\'');
 
-  assert.notEqual(color2, color1);
+  Ember.run(function(){
+    component.set('style', 'success');
+  });
+  assert.ok(this.$().hasClass('btn-success'), 'it should have \'btn-success\' class when style is set to \'success\'');
+
+  Ember.run(function(){
+    component.set('style', 'danger');
+  });
+  assert.ok(this.$().hasClass('btn-danger'), 'it should have \'btn-danger\' class when style is set to \'danger\'');
+
+  Ember.run(function(){
+    component.set('style', 'warning');
+  });
+  assert.ok(this.$().hasClass('btn-warning'), 'it should have \'btn-warning\' class when style is set to \'warning\'');
+
+  Ember.run(function(){
+    component.set('style', 'info');
+  });
+  assert.ok(this.$().hasClass('btn-info'), 'it should have \'btn-info\' class when style is set to \'info\'');
+
+  Ember.run(function(){
+    component.set('style', 'link');
+  });
+  assert.ok(this.$().hasClass('btn-link'), 'it should have \'btn-link\' class when style is set to \'link\'');
+});
+
+test('it changes size when size property is set', function(assert) {
+  assert.expect(3);
+  var component = this.subject();
+  this.render();
+
+  Ember.run(function(){
+    component.set('size', 'lg');
+  });
+  assert.ok(this.$().hasClass('btn-lg'), 'it should have \'btn-lg\' class when size is set to \'lg\'');
+
+  Ember.run(function(){
+    component.set('size', 'sm');
+  });
+  assert.ok(this.$().hasClass('btn-sm'), 'it should have \'btn-sm\' class when size is set to \'sm\'');
+
+  Ember.run(function(){
+    component.set('size', 'xs');
+  });
+  assert.ok(this.$().hasClass('btn-xs'), 'it should have \'btn-xs\' class when size is set to \'xs\'');
 });
