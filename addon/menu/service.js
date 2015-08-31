80ff07687df2118c1582f0720df40b20d7e4a65f
Kyle <kyle.scherling@revelationglobal.com>
2015-08-31 10:07:15 -0700

    Adds cookie addon, adds initializer, injects into menu service, adds basic configuration

--- addon/menu/service.js
+++ addon/menu/service.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  // TBD: May rename as layout-preferences
+  // Dependency: ember-cli-cookie
+  // Inititialized and injected directly to this service
+  // TBD: May rename this as layout-preferences
   unfixMenu: false,
   showMenu: false,
 
+  unfixMenuObserver: Ember.observer('unfixMenu', function(){
+    var cookie = this.get('cookie');
+    cookie.setCookie('userprefsmenuunfix', this.get('unfixMenu'));
+  }),
+
   toggleFixedMenu() {
-    this.set('showMenu', false);
     this.toggleProperty('unfixMenu');
   },
 
---
+++ tests/dummy/app/initializers/ember-cli-cookie.js
@@ -0,0 +1,11 @@
+// https://www.npmjs.com/package/ember-cli-cookie
+
+export function initialize(container, app) {
+  app.inject('service:menu', 'cookie', 'cookie:main');
+}
+
+export default {
+  name: 'ember-cli-cookie',
+  after: ['cookie'],
+  initialize: initialize
+}
