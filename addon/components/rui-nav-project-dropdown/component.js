785607a2c94fb802431f555c275ae2cb2e488f1a
Kyle <kyle.scherling@revelationglobal.com>
2015-09-01 12:18:30 -0700

    Moves dropdown into component

--- addon/components/rui-nav-project-dropdown/component.js
+++ addon/components/rui-nav-project-dropdown/component.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import layout from './template';
-import RuiDropdownComponent from 'ember-cli-revelation-ui/components/rui-dropdown/component';
 
-export default RuiDropdownComponent.extend({
+export default Ember.Component.extend({
   layout: layout
 });
