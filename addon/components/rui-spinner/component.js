373f9163045d38cc0e53a4298172b569be6e4a6b
David Zulaica <zulaica@gmail.com>
2015-09-09 11:32:24 -0700

    - Add size attribute to rui-spinner.

--- addon/components/rui-spinner/component.js
+++ addon/components/rui-spinner/component.js
@@ -4,5 +4,28 @@ import layout from './template';
 export default Ember.Component.extend({
   layout: layout,
   tagName: 'i',
-  classNames: ['fa', 'fa-spin', 'fa-circle-o-notch']
+  classNames: ['fa', 'fa-spin', 'fa-circle-o-notch'],
+  classNameBindings: ['sizeComputed'],
+
+  // Defaults
+  size: null,
+  sizes: ['lg'],
+
+  // Computed
+  sizeComputed: Ember.computed('size', function() {
+    var sizes = this.get('sizes');
+    var findSize = this.get('size');
+    var resolvedSize = findSize;
+
+    if (!findSize) {
+      return null;
+    }
+
+    if (sizes.indexOf(findSize) === -1) {
+      Ember.Logger.warn('rui-spinner: You specified an unsupported \'size\' property, so we\'ve set it to the standard size. Please either omit the attribute for standard size or choose from one of the following: [\'lg\'].');
+      return null;
+    }
+
+    return 'rui-spinner-' + resolvedSize;
+  })
 });
