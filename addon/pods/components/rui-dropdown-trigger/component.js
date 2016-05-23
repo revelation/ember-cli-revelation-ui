ce0738734f255a3ece9d4ec07f12b456982bb427
ksure <kyle.scherling@gmail.com>
2016-05-23 16:58:20 -0700

    fixes some styling issues

--- addon/pods/components/rui-dropdown-trigger/component.js
+++ addon/pods/components/rui-dropdown-trigger/component.js
@@ -1,23 +1,29 @@
-import Ember from 'ember';
-import layout from './template';
+import Ember from 'ember'
+import layout from './template'
 
-export default Ember.Component.extend({
+const {
+  Component,
+  computed,
+  Logger
+} = Ember
+
+export default Component.extend({
   layout: layout,
   tagName: 'button',
   classNames: ['dropdown-toggle'],
   classNameBindings: ['computedStyle'],
   attributeBindings: [
-    'type',
-    'data-toggle',
-    'aria-haspopup',
     'aria-expanded',
-    'data-test-id'
+    'aria-haspopup',
+    'data-test-id',
+    'data-toggle',
+    'type'
   ],
 
   //Defaults
-  type: 'button',
-  style: null,
   size: null,
+  style: null,
+  type: 'button',
 
   //Constructors
   classPrefix: 'btn',
@@ -31,23 +37,23 @@ export default Ember.Component.extend({
   'aria-expanded': 'false',
 
   // Computed
-  computedStyle: Ember.computed('style', function(){
+  computedStyle: computed('style', function(){
     // Builds size button class
     // Should not have any btn class by default to allow for custom classing
 
     if (!this.get('style')) {
-      return false;
+      return false
     }
 
-    var styles = this.get('styles');
-    var findStyle = this.get('style');
-    var resolvedStyle = findStyle;
+    const styles = this.get('styles')
+    const findStyle = this.get('style')
+    const resolvedStyle = findStyle
 
     if (styles.indexOf(findStyle) === -1) {
-      resolvedStyle = 'secondary';
-      Ember.Logger.warn('rui-dropdown-trigger: You specified and unsupported \'style\' property so we\'ve defaulted to the secondary style: choose from \'primary secondary success warning danger info link.\'');
+      resolvedStyle = 'secondary'
+      Logger.warn('rui-dropdown-trigger: You specified and unsupported \'style\' property so we\'ve defaulted to the secondary style: choose from \'primary secondary success warning danger info link.\'')
     }
 
-    return 'btn ' + this.get('classPrefix') + '-' + resolvedStyle;
+    return 'btn ' + this.get('classPrefix') + '-' + resolvedStyle
   }),
-});
+})
