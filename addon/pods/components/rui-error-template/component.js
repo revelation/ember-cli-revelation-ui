4b556330c6481bc27b4173155052d2f459e4b7ab
Kyle Mellander <kmellander@focusvision.com>
2016-05-23 17:04:02 -0700

    Rename error-template classes to rui-error-template

--- addon/pods/components/rui-error-template/component.js
+++ addon/pods/components/rui-error-template/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember'
 import layout from './template'
 
 export default Ember.Component.extend({
-  classNames: ['error-template'],
+  classNames: ['rui-error-template'],
   layout,
 
   actions: {
