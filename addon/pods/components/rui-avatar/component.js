94f1b44ce0ed8cf27fc5c1c427628ddf26398c2a
ksure <kyle.scherling@gmail.com>
2016-06-06 17:31:36 -0700

    Adds default placeholder avatar image

--- addon/pods/components/rui-avatar/component.js
+++ addon/pods/components/rui-avatar/component.js
@@ -1,29 +1,22 @@
-import Ember from 'ember';
-import layout from './template';
+import Ember from 'ember'
+import layout from './template'
 
-export default Ember.Component.extend({
+const {
+  Component,
+  computed,
+  Logger
+} = Ember
+
+export default Component.extend({
   layout: layout,
-  tagName: 'img',
-  attributeBindings: ['computedSrc:src'],
+  tagName: 'span',
   classNames: ['rui-avatar'],
   classNameBindings: ['sizeComputed'],
 
-  // Defaults
-  computedSrc: Ember.computed('src', function() {
-    // `src` is supplied
-    if (this.get('src') !== '') {
-      return this.get('src');
-    }
-    // `src` is empty
-    if (this.get('src') === '') {
-      Ember.Logger.warn('rui-avatar: You did not specify a `src`, so we have set it to a placeholder image. Please specify a `src`.');
-      return 'http://placehold.it/32?text=%20';
-    }
-  }),
-  src: Ember.computed('src', function() {
-    // `src` is not supplied
-    Ember.Logger.warn('rui-avatar: You did not specify a `src`, so we have set it to a placeholder image. Please specify a `src`.');
-    return 'http://placehold.it/32?text=%20';
+  src: '', //passed in
+  showPlaceholder: computed('src', function() {
+    const src = this.get('src')
+    return src === undefined || src === ''
   }),
 
   // Constructors
@@ -31,20 +24,20 @@ export default Ember.Component.extend({
   sizes: ['lg', 'sm'],
 
   // Computed
-  sizeComputed: Ember.computed('size', function() {
-    var sizes = this.get('sizes');
-    var findSize = this.get('size');
-    var resolvedSize = findSize;
+  sizeComputed: computed('size', function() {
+    const sizes = this.get('sizes')
+    const findSize = this.get('size')
+    const resolvedSize = findSize
 
     if (!findSize) {
-      return null;
+      return null
     }
 
     if (sizes.indexOf(findSize) === -1) {
-      Ember.Logger.warn('rui-avatar: You specified an unsupported \'size\' property, so we\'ve set it to the standard size. Please either omit the attribute for standard size or choose from one of the following: [\'lg\', \'sm\'].');
-      return null;
+      Logger.warn('rui-avatar: You specified an unsupported \'size\' property, so we\'ve set it to the standard size. Please either omit the attribute for standard size or choose from one of the following: [\'lg\', \'sm\'].')
+      return null
     }
 
-    return this.get('classPrefix') + '-' + resolvedSize;
+    return this.get('classPrefix') + '-' + resolvedSize
   })
-});
+})
