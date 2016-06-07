a753529bc27b4057c7ffc129b66bdd7b2799aa94
ksure <kyle.scherling@gmail.com>
2016-06-06 17:52:47 -0700

    Removes some confusing variables

--- addon/pods/components/rui-avatar/component.js
+++ addon/pods/components/rui-avatar/component.js
@@ -26,18 +26,17 @@ export default Component.extend({
   // Computed
   sizeComputed: computed('size', function() {
     const sizes = this.get('sizes')
-    const findSize = this.get('size')
-    const resolvedSize = findSize
+    const size = this.get('size')
 
-    if (!findSize) {
+    if (!size) {
       return false
     }
 
-    if (sizes.indexOf(findSize) === -1) {
+    if (sizes.indexOf(size) === -1) {
       Logger.warn('rui-avatar: You specified an unsupported \'size\' property, so we\'ve set it to the standard size. Please either omit the attribute for standard size or choose from one of the following: [\'lg\', \'sm\'].')
       return false
     }
 
-    return this.get('classPrefix') + '-' + resolvedSize
+    return this.get('classPrefix') + '-' + size
   })
 })
