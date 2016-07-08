b13c86d101e74b9652d7e062f3e32cd84f891a19
Kyle Mellander <kmellander@focusvision.com>
2016-07-08 15:10:24 -0700

    Update tests to pass

--- tests/unit/components/rui-icon-test.js
+++ tests/unit/components/rui-icon-test.js
@@ -21,3 +21,23 @@ test('it renders', function(assert) {
     'it renders with the base class `fa`'
   );
 });
+
+test('it updates when settings change', function(assert) {
+  assert.expect(2)
+  var component = this.subject()
+  component.set('name', 'times')
+
+  assert.equal(
+    component.get('nameComputed'),
+    'fa-times',
+    'name is times on initial render'
+  )
+
+  component.set('name', 'check')
+
+  assert.equal(
+    component.get('nameComputed'),
+    'fa-check',
+    'name is updated to check when computed'
+  )
+})
